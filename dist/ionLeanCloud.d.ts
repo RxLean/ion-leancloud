38ad80e734812717815e27e266012994c082c4a5
WuJun <wujun19890209@gmail.com>
2017-05-16 10:55:35 +0800

    chore: npm ignore.

--- dist/ionLeanCloud.d.ts
+++
@@ -1,9 +0,0 @@
-import { IWebSocketClient, IStorage } from 'leancloud-typescript-rx-sdk';
-export declare class IonLeanCloud {
-    private static _WebSocketProvider;
-    static readonly ionWebSocketClient: IWebSocketClient;
-    private static _StorageProvider;
-    static readonly ionStorage: IStorage;
-    private static _DeviceProvider;
-    static ionDevice: any;
-}
